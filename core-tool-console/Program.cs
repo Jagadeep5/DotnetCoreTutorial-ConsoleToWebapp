1c2a85438f9037251ade354e79420fbbbf714ed3
Jagadeep5 <Jagadeep5@live.com>
2021-05-15 00:17:47 +0530

    first commit

---
+++ core-tool-console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace core_tool_console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// We converted this from console app to web app
+			
+			CreateWebHostBilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateWebHostBilder(string[] args)
+		{
+			return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(x =>
+			{
+				x.UseStartup<StartupCustom>();
+			});
+		}
+	}
+}
---
+++ core-tool-console/StartupCustom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace core_tool_console
+{
+	class StartupCustom
+	{
+		public void ConfigureServices()
+		{
+
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			app.Use( async (context, a) =>
+			{
+				await context.Response.WriteAsync("Hello");
+			});
+		}
+	}
+}
